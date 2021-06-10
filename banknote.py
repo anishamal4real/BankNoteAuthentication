27a9dec323fe46132fa9631e54dea0cecd8c210a
Anis Hamal Blanco <anishamal4real@gmail.com>
2021-06-10 21:40:27 +0545

    Added predict_file

--- banknote.py
+++ banknote.py
@@ -2,12 +2,18 @@ from flask import Flask,request
 import pandas as pd
 import numpy as np
 import pickle
+import flasgger
+from flasgger import Swagger
+
 app=Flask(__name__)
+Swagger(app)
+
 pickle_in= open('classifier.pkl','rb')
 classifier=pickle.load(pickle_in)
 @app.route('/')
 def welcome():
-    return "Welcome All"
+    return "WELCOME TO THE BLANCO WORLD WHERE NOTHING IS REALLY PRETTY "
+
 @app.route('/predict/')
 def predict_note():
     variance= request.args.get('variance')
@@ -22,16 +28,19 @@ def predict_note():
 if __name__=='__main__':
     app.run()
 
-
-
-
-@app.route('/predict_file',methods=["POST","GET"])
+@app.route('/predict_form',methods=["GET"])
+def predict_note_authentication(variance,skewness,curtosis,entropy):
+    prediction=classifier.predict([[variance,skewness,curtosis,entropy]])
+    print(prediction)
+    return prediction)
+@app.route('/predict_file',methods=["POST"])
 def predict_note_file():
     df_test=pd.read_csv(request.files.get("file"))
     print(df_test.head())
     prediction=classifier.predict(df_test)
-    
     return str(list(prediction))
 
-if __name__=='__main__':
-    app.run()
+
+
+
+
